0ec283b50860443a2f76422bd928017ca4d9b3e7
Dave Manton <dave@mrmanton.com>
2015-01-31 23:41:12 +0000

    Update SetupScreen.java

--- SetupScreen.java
+++ SetupScreen.java
@@ -4,25 +4,36 @@ import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-
+/**
+ * <h1>SetupScreen Object</h1>
+ * Creates the screen layout for setting up the world game state.
+ * 
+ * @author <a href="mailto:dave@mrmanton.com">Dave Manton</a>
+ * @see <a href="https://github.com/davemanton/GameOfLife">GitHub Project Repository</a>
+ */
 public class SetupScreen extends JPanel{
 
 	private ClickableCell[][] worldSetup;
-	private JPanel worldView, header;
+	private JPanel worldView, footer;
 	
-	public SetupScreen (int rows, int columns, JButton action) {
+	//Standard Constructor
+	/**
+	 * Creates the setup screen object
+	 * 
+	 * @param rows		The number of rows in the world
+	 * @param columns	The number of columns in the world
+	 * @param action	A button to be used on the screen
+	 */
+	public SetupScreen (int rows, int columns, JButton action) {		
 		
 		setLayout(new BorderLayout());
-		header = new JPanel();
-		header.add(action);
-		add(header, BorderLayout.SOUTH);
-		
 		
 		//Create World View
 		worldView = new JPanel();
 		worldView.setLayout(new GridLayout(rows, columns));
 		worldSetup = new ClickableCell[rows][columns];
 		
+		//Add ClickableCells
 		for (int i=0; i<worldSetup.length; i++) {
 			for (int j=0; j<worldSetup[i].length; j++) {
 				worldSetup[i][j] = new ClickableCell();
@@ -32,8 +43,18 @@ public class SetupScreen extends JPanel{
 		}
 		
 		add(worldView, BorderLayout.CENTER);
-	}
+		
+		//Add footer with controls
+		footer = new JPanel();
+		footer.add(action);
+		add(footer, BorderLayout.SOUTH);
+	}//end standard constructor
 	
+	/**
+	 * Gives the selected starting state of the game world
+	 * 
+	 * @return	Returns a 2d boolean array representing the game state
+	 */
 	public boolean[][] getSetupState() {
 		boolean[][] setupState = new boolean[worldSetup.length][worldSetup[0].length];
 		
@@ -44,5 +65,6 @@ public class SetupScreen extends JPanel{
 		}
 		
 		return setupState;
-	}
-}
+	}//end getSetupState method
+	
+}//end SetupScreen class

76833c4255bcc641ae99e4a2aa55df86ea3b5cf3
Dave Manton <dave@mrmanton.com>
2015-01-31 23:44:19 +0000

    Update WorldScreen.java

--- WorldScreen.java
+++ WorldScreen.java
@@ -7,7 +7,13 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-
+/**
+ * <h1>WorldScreen Object</h1>
+ * Defines the display of the cellular world defined in the game.
+ * 
+ * @author <a href="mailto:dave@mrmanton.com">Dave Manton</a>
+ * @see <a href="https://github.com/davemanton/GameOfLife">GitHub Project Repository</a>
+ */
 public class WorldScreen extends JPanel{
 	
 	private JPanel footer;
@@ -22,6 +28,14 @@ public class WorldScreen extends JPanel{
 	
 	private World world;
 
+	//Constructor method
+	/**
+	 * Standard constructor for the WorldScreen display panel.
+	 * 
+	 * @param rows		The number of rows of cells to be displayed
+	 * @param columns	The number of columns of cells to be displayed
+	 * @param restart	A button for placement on screen
+	 */
 	public WorldScreen (int rows, int columns, JButton restart) {
 		
 		setLayout(new BorderLayout());
@@ -31,20 +45,20 @@ public class WorldScreen extends JPanel{
 		worldView.setLayout(new GridLayout(rows, columns));
 		worldCells = new DisplayCell[rows][columns];
 		
+		//Create cells on screen
 		for (int i=0; i<worldCells.length; i++) {
 			for (int j=0; j<worldCells[i].length; j++) {
 				worldCells[i][j] = new DisplayCell();
 				
 				worldView.add(worldCells[i][j]);
-			}
-		}
+			}//end columns loop
+		}//end rows loop
 		
 		add(worldView, BorderLayout.CENTER);
 		
-		
+		//Create footer with controls
 		footer = new JPanel();
 		footer.add(restart);
-		
 		
 		nextGen = new JButton("Next Generation");
 		nextGen.addActionListener(new ActionListener() {
@@ -59,41 +73,46 @@ public class WorldScreen extends JPanel{
 		footer.add(generationLb);
 		
 		
-		add(footer, BorderLayout.SOUTH);
-		
-		
-		
-	}
+		add(footer, BorderLayout.SOUTH);		
+	}//end standard constructor
 	
+	//Transformer Methods
+	/**
+	 * Creates the world based on the input from setup screen
+	 * 
+	 * @param worldState	A 2D boolean array
+	 */
 	public void setupWorld(boolean[][] worldState) {
 		
 		world = new World(worldCells.length, worldCells[0].length, worldState);
 		
 		updateDisplay(worldState);
-	}
+	}//end setupWorld transformer method
 	
+	//Update display based on 2d boolean array
 	private void updateDisplay(boolean[][] worldState) {
 		for (int i=0; i<worldState.length; i++) {
 			for (int j=0; j<worldState.length; j++) {
 				worldCells[i][j].setState(worldState[i][j]);
-			}
-		}
+			}//end columns loop
+		}//end rows loop
 		
 		generationLb.setText("Generation: " + genCount);
-	}
+	}//end updateDisplay method
 	
+	//Create next generation
 	private void nextGeneration() {
+		//Create a generation based on world status
 		Generation current = new Generation(world);
-		
+		//Create an evolution
 		Evolution nextGen = new Evolution(current);
-		
+		//Create the next generation
 		Generation future = nextGen.newEvolution();
-		
+		//Update the world state
 		world.nextGeneration(future);
-		
+		//Update the display
 		genCount++;
 		updateDisplay(future.getGeneration());
-	}
-	
-	
-}
+	}//end nextGeneration method
+		
+}//end WorldScreen class

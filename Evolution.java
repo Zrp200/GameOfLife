a222a0b5e5d782b5e45a841ed8f1063acadafabd
Dave Manton <dave@mrmanton.com>
2015-01-31 23:38:08 +0000

    Update Evolution.java

--- Evolution.java
+++ Evolution.java
@@ -1,19 +1,35 @@
-
+/**
+ * <h1>Evolution Object</h1>
+ * Controls the evolution from one generation to another
+ * 
+ * @author <a href="mailto:dave@mrmanton.com">Dave Manton</a>
+ * @see <a href="https://github.com/davemanton/GameOfLife">GitHub Project Repository</a>
+ */
 public class Evolution {
 	
 	private Generation original;
 	private Generation evolution;
 
+	/**
+	 * Constructor of evolution object
+	 * 
+	 * @param origins	The original generation state of the game
+	 */
 	public Evolution(Generation origins) {
 		original = origins;
 		evolution = new Generation(origins);
-	}
+	}//end standard constructor method
 	
+	/**
+	 * Causes evolution from one generation to another
+	 * 
+	 * @return		Returns the next evolution of the generation
+	 */
 	public Generation newEvolution() {
 		boolean workingCellState, newCellState;
 		Neighbours workingCellNeighbours;
 		
-		
+		//Works through each cell of generation
 		for(int i=0; i<original.getRowsNo(); i++) {
 			for(int j=0; j<original.getColumnsNo(); j++) {
 				
@@ -33,11 +49,13 @@ public class Evolution {
 		}
 		
 		return evolution;
-	}
+	}//end newEvolution method
 	
+	//Calculates neighbours of a given cell
 	private Neighbours calculateNeighbours(int rowRef, int colRef) {
 		Neighbours temp = new Neighbours();
 		
+		//Works out potential neighbours based on position in boolean array
 		if(rowRef>0) {
 			if(colRef>0) temp.addNeighbour(original.getGenerationElement(rowRef-1, colRef-1));
 			temp.addNeighbour(original.getGenerationElement(rowRef-1, colRef));
@@ -54,14 +72,17 @@ public class Evolution {
 		}
 		
 		return temp;
-	}
+	}//end calculateNeighbours method
 	
+	//Calculates status of a given cell based on live numbers of cell
 	private boolean calculateStatus(boolean cell, int livingNeighbours) {
 		
+		//Rules 1,2 and 3 covered by whether too many or too few neighbours
 		if((livingNeighbours<2 || livingNeighbours>3) && cell==true) cell=false;
+		//Rule 4 to bring cells back to life if exactly 3 neighborus
 		else if(livingNeighbours==3 && cell==false) cell=true;
 		
 		return cell;
-	}
+	}//end calculateStatus method
 
-}
+}//end Evolution class

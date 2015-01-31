5184410037475d16700daafc716bac9da5ce6f15
Dave Manton <dave@mrmanton.com>
2015-01-31 23:34:19 +0000

    Update ClickableCell.java

--- ClickableCell.java
+++ ClickableCell.java
@@ -5,23 +5,34 @@ import java.awt.event.MouseEvent;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
-
+/**
+ * <h1>ClickableCell Object</h1>
+ * Extends DisplayCell to create a cell which can be toggled by clicking
+ * Allows the definition of a game state by clicking on cells to define 
+ * whether their living status.
+ * <p>Blank=Alive, White=Dead</p>
+ * 
+ * @author <a href="mailto:dave@mrmanton.com">Dave Manton</a>
+ * @see <a href="https://github.com/davemanton/GameOfLife">GitHub Project Repository</a>
+ */
 public class ClickableCell extends DisplayCell {
 
-	
+	/**
+	 * Default constructor
+	 */
 	public ClickableCell() {
-		
+		//Uses DisplayCell constructor
 		super();
-		
+		//Creates listener object for cell
 		addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				toggleState();
 			}
-		});
-		
-	}		
+		});	
+	}//end default constructor		
 	
-	public void toggleState() {
+	//Toggle living state method for click event
+	private void toggleState() {
 		if(state) {
 			state=false;
 			setBackground(Color.white);
@@ -29,6 +40,6 @@ public class ClickableCell extends DisplayCell {
 			state=true;
 			setBackground(Color.black);
 		}
-	}
+	}//end toggleState method
 	
 }//end ClickableCell class
